56cea97422faa0380a76f2c67fde5c74236abaf9
Roshan Bhattarai <roshan.bhattarai@proroshan.com>
2024-01-03 02:29:55 +1100

    working on Canvas API

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
-console.log("Hello World");
+const rulesBtnElm = document.getElementById('rules-btn');
+const closeBtnElm = document.getElementById('close-btn');
+const rulesElm = document.getElementById('rules');
+
+
+rulesBtnElm.addEventListener('click', ()=>{
+
+    rulesElm.classList.add('show')
+    rulesBtnElm.remove()
+
+})
+
+
+closeBtnElm.addEventListener('click', ()=>{
+    rulesElm.classList.remove('show')
+    const newRulesBtnElm = document.createElement('button');
+    newRulesBtnElm.id = 'rules-btn';
+    newRulesBtnElm.className = 'btn rules-btn'
+    newRulesBtnElm.innerText = 'Show Rules';
+
+    // Insert the newRulesBtnElm back into the DOM after the rulesElm
+    rulesElm.insertAdjacentElement('afterend', newRulesBtnElm);
+
+    // Add event listener to the newRulesBtnElm
+    newRulesBtnElm.addEventListener('click', () => {
+        rulesElm.classList.add('show');
+        newRulesBtnElm.remove();
+    });
+    
+   
+
+})
